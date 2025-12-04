7c61a01e00a3e249cfe687940d4bd2bd717feca8
murted90 <148001605+murted90@users.noreply.github.com>
2025-12-05 01:01:51 +0300

    Add export functionality for reports

--- utils.js
+++ utils.js
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2025-12-04T22:01:42.309Z
-Commit: Fix CSS styling bug on mobile devices
+Timestamp: 2025-12-04T22:01:51.533Z
+Commit: Add export functionality for reports
